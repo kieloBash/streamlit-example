9e2744456a7d0d14adde5b816cb3a953c8447346
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2024-04-10 23:21:01 +0800

    template commit

---
+++ image_stitching.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def stitch_image():
+    st.write("stitch image")
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import image_stitching as ist
 
 """
 # Welcome to Streamlit!
@@ -14,3 +15,21 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 st.subheader("Hi, I am Kielo")
+
+# Upload multiple image files
+uploaded_files = st.file_uploader("Upload two images", type=['png', 'jpg', 'jpeg'], accept_multiple_files=True)
+
+if uploaded_files is not None:
+    for uploaded_file in uploaded_files:
+        # Display each uploaded image
+        st.image(uploaded_file)
+        
+        
+        
+def my_function():
+    st.write("Button was clicked!")
+
+# Create a button
+if st.button('Click me'):
+    # Call the function when the button is clicked
+    ist.stitch_image()
